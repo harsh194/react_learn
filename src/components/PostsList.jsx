37734e2088f01e32a1de16377665fdf5f78051e8
Harsh Ranjan <133637871+harsh194@users.noreply.github.com>
2024-01-25 14:28:39 +0900

    add router

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NewPost from './NewPost';
 import Post from './Post';
 import Modal from './Modal';
@@ -9,8 +9,26 @@ function PostsList({isPosting, onStopPosting})
 {
     const[posts, setPosts] = useState([]);
 
+    useEffect(() => {
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts')
+            const resData = await response.json();
+            setPosts(resData.posts);
+        }
+        fetchPosts();
+    }, []);
+
+    //const [posts, setPosts] = useState([]);
     function addPostHandler(postData)
     {
+        fetch('http://localhost:8080/posts',
+        {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        });
         setPosts((existingPosts) => [postData, ...existingPosts]);
     }
 
